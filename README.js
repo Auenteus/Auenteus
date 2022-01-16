fa8cb1c7ceb64162cd3b40779444a166dfed8f30
Alone <97841007+Auenteus@users.noreply.github.com>
2022-01-16 18:43:50 +0300

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,38 @@
+       class Auenteus {
+       constructor(...options) {
+    
+       this.height = "1.77"
+       this.weight = "67"
+       this.type = "human"
+       this.job = "student"
+       this.sex = "male"
+      }
+    }
+
+      class CreateMan extends Auenteus {
+      constructor(...options) {
+      super(options);
+    }
+  
+     private _eating() {
+     void "eating 🍔 🍟 🍗 🥤"
+    }
+  
+     private _coding() {
+      void "coding... ❤️"
+    }
+  
+     private _sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
+  
+        async createDay() {
+        this._eating()
+        this._coding()
+        await this._sleep(18000000)
+    
+          this.createDay()
+      }
+  
+    }
+
+    let Auenteus = new CreateMan()
+    Auenteus.createDay();
